b4797dd74652af7777b242ebed31f9e79e81a900
Fabien Lim Zheng Kai <fabienlimzk@gmail.com>
2020-08-20 00:00:24 +0800

    part-three

--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -4,19 +4,20 @@ import Fave from "./Fave"
 
 export default class FilmRow extends Component {
   handleDetailsClick = (film) => {
-    console.log("Fetching details for " + film);
+    // console.log(`Fetching details for ${film.title}`);
+    this.props.onDetailsClick(film);
   }
   
   render() {
-    const fullYear = new Date(this.props.movie.release_date);
+    const fullYear = new Date(this.props.film.release_date);
     return (
-      <div className="film-row" onClick={() => this.handleDetailsClick(this.props.movie.title)}>
-        <FilmPoster picture={this.props.movie.poster_path} />
+      <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film.title)}>
+        <FilmPoster picture={this.props.film.poster_path} />
         <div className="film-summary">
-          <h1>{this.props.movie.title}</h1>
+          <h1>{this.props.film.title}</h1>
           <p>{fullYear.getFullYear()}</p>
         </div>
-        <Fave />
+        <Fave onFaveToggle={this.props.onFaveToggle} isFave={this.props.isFave} />
       </div>
     )
   }
